e5c1fdb1561069c954e181da3328ce5c3746ab29
amaurycarvalho <105219866+amaurycarvalho@users.noreply.github.com>
2022-05-09 12:53:00 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-bzr whoami "Amaury Carvalho <amauryspires@gmail.com>"
-bzr launchpad-login amaurycarvalho
-#bzr init
-bzr add *
-today=$(date +"%x %r %Z")
-bzr commit -m "Commit on $today by $USER"
-bzr push lp:~amaurycarvalho/msxbas2rom/trunk
