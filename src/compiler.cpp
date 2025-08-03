c9d780a77feca35a2c7957ba1908aa85ed70b4be
Amaury <amauryspires@gmail.com>
2025-08-03 19:33:14 -0300

    Code documentation

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,7 +1,7 @@
 /***
  * @file compiler.cpp
  * @brief Compiler class code for semantic analysis,
- * specialized as a Z80 code builder for MSX system
+ *        specialized as a Z80 code builder for MSX system
  * @author Amaury Carvalho (2019-2025)
  * @note
  *   https://en.wikipedia.org/wiki/Semantic_analysis_(computational)
