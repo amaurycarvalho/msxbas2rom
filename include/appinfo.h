fb50267207aa0b8117d6f154f6d51d2167918afe
Amaury <amauryspires@gmail.com>
2025-09-14 13:23:06 -0300

    refactor: Resources refactoring and limit expanded (~3200)

--- include/appinfo.h
+++ include/appinfo.h
@@ -73,7 +73,7 @@ https://github.com/amaurycarvalho/msxbas2rom/wiki/Usage
 const char *info_history = R"(
 Version history
 
-0.3.3.2 – (2025/09/##) Resources refactoring and limit expanded (~3200), 
+0.3.3.2 – (2025/09/14) Resources refactoring and limit expanded (~3200), 
                        NoICE OpenMSX Debugger support and minor bug fixes;
 0.3.3.1 – (2025/09/03) Symbols/resources refactoring, OMDS support,
                        CLEAR statement and minor bug fixes;
