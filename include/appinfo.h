5bc203d8b1253af05be07c7e1b4bc8b497fde711
Amaury <amauryspires@gmail.com>
2025-09-26 16:37:09 -0300

    refactor: MTF map loading optimization

--- include/appinfo.h
+++ include/appinfo.h
@@ -73,7 +73,7 @@ https://github.com/amaurycarvalho/msxbas2rom/wiki/Usage
 const char *info_history = R"(
 Version history
 
-0.3.3.4 – (2025/10/26) MSX Tile Forge map loading optimization and 
+0.3.3.4 – (2025/09/26) MSX Tile Forge map loading optimization and 
                        resource file check bug fix;
 0.3.3.3 – (2025/09/20) MSX Tile Forge support (CMD MTF);
 0.3.3.2 – (2025/09/14) Resources refactoring and limit expanded (~3200), 
